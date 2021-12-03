8ab98fd9a9834c23b351b2cd4ba4944dff2858ad
alecasa00 <alecasa2000@gmail.com>
2021-12-03 10:41:00 +0100

    inizio sviluppo...

--- src/main/java/Spettacoli.java
+++ src/main/java/Spettacoli.java
@@ -10,4 +10,46 @@ public class Spettacoli {
     private double prezzo;
     private int durata, id;
 
+    public Spettacoli(String nome, String nomeSala, String genere, Date orario, Date giorno, double prezzo, int durata, int id) {
+        this.nome = nome;
+        this.nomeSala = nomeSala;
+        this.genere = genere;
+        this.orario = orario;
+        this.giorno = giorno;
+        this.prezzo = prezzo;
+        this.durata = durata;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNomeSala() {
+        return nomeSala;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public Date getOrario() {
+        return orario;
+    }
+
+    public Date getGiorno() {
+        return giorno;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+
+    public int getId() {
+        return id;
+    }
 }

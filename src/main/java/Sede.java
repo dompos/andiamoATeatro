61c127ef34d6640f98ba80a777f53592f48d7e2d
Domenico Posillipo <domposi@gmail.com>
2021-12-06 09:19:54 +0100

    Completato metodo isDisponibile

--- src/main/java/Sede.java
+++ src/main/java/Sede.java
@@ -1,6 +1,6 @@
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.*;
 
 public class Sede {
     private String nome;
@@ -17,8 +17,43 @@ public class Sede {
         this.coperto = coperto;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public String getComune() {
+        return comune;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isCoperto() {
+        return coperto;
+    }
+
     public boolean isDisponibile(Connection conn, String spettacolo) throws SQLException {
-        return true;
+        Set<Spettacoli> spettacoliSet = new HashSet();
+        List<Sala> sale = Servizi.scaricaSale(
+                conn,
+                new ArrayList<>(Collections.singleton("sede_id = "+ id)));
+        for (Sala s : sale){
+            List<Spettacoli> spettacoli = Servizi.scaricaSpettacoli(
+                    conn,
+                    new ArrayList<>(Collections.singleton("sala_nome = '" + s.getNome() + "'")));
+            spettacoliSet.addAll(spettacoli);
+        }
+        for (Spettacoli setSpettacolo : spettacoliSet){
+            if (setSpettacolo.getNome().equalsIgnoreCase(spettacolo) &&
+                Servizi.scaricaSala(conn, setSpettacolo.getNomeSala()).getnPosti() > 1)
+                return true;
+        }
+        return false;
     }
 
     @Override

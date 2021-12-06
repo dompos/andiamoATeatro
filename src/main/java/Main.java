d946d5e6ddd89fbeaa26ae0efb2941cf23f183e1
Domenico Posillipo <domposi@gmail.com>
2021-12-06 09:30:04 +0100

    Aggiunte righe di test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,8 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
         try(Connection conn = DriverManager.getConnection(Servizi.URL, Servizi.USERNAME, Servizi.PASSWORD)){
-            Servizi.scaricaUtente(conn,"domposi@gmail.com");
-            Servizi.scaricaSede(conn,1);
+            Servizi.scaricaUtente(conn,"domposi@hotmail.it");
+            Sede sede = Servizi.scaricaSede(conn,1);
+            System.out.println(sede.isDisponibile(conn, "batman"));
             Servizi.scaricaSala(conn,"Sala Debora");
             Servizi.scaricaPrenotazione(conn,1);
             Servizi.scaricaPosti(conn,1);
@@ -16,7 +17,11 @@ public class Main {
             Utente utente = new Utente("Davide", "Golino", "Roma", "dasuaphs@blizzard.com", "3694221564");
             System.out.println(Servizi.caricaUtente(conn,utente));
             Servizi.scaricaUtente(conn,"dasuaphs@blizzard.com");
-            Servizi.scaricaUtente(conn,"dompos@hotmail.it");
+            Servizi.scaricaUtente(conn,"dompos@outlook.com");
+            Prenotazioni prenotazione = new Prenotazioni(2,1,1,"domposi@hotmail.it");
+            Servizi.caricaPrenotazione(conn, prenotazione);
+            Servizi.scaricaPrenotazione(conn, 2);
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
